c2610eea00cd3af76a31320ed848a6f1bb7ea982
ahungrynoob <dxd_sjtu@outlook.com>
2021-10-28 19:31:41 +0800

    fix:  index.js useless method

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const { loadBinding } = require('@node-rs/helper')
 
-const {
-  compile: _compile,
-  isValid: _isValid,
-  validate: _validate,
-} = loadBinding(__dirname, 'jsonschema', '@node-rs/jsonschema')
+const { compile: _compile } = loadBinding(__dirname, 'jsonschema', '@node-rs/jsonschema')
 
 module.exports.compile = function (schema) {
-  // const obj = {
-  //   _compile,
-  //   isValid: (input) => _isValid.call(obj, JSON.stringify(input)),
-  //   validate: (input) => _validate.call(obj, JSON.stringify(input)),
-  // }
-  // obj._compile(JSON.stringify(schema))
-  // return obj
   return _compile(JSON.stringify(schema))
 }
